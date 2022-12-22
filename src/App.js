a242271bbe1efcf6f9e380e782b45bc589694a68
diogenispanagiotis <diogenis.panagiotis@gmail.com>
2022-12-22 11:38:41 -0500

    reformat

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import CreateNote from './CreateNote';
+import Labels from './Labels';
 import Note from './Note';
 import { useState, useEffect } from 'react';
 import { db } from './firebase-config.js';
-import { collection, doc, addDoc, deleteDoc, updateDoc, onSnapshot, query, orderBy, serverTimestamp } from 'firebase/firestore';
+import { 
+  collection,
+  doc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  onSnapshot,
+  query,
+  orderBy,
+  serverTimestamp
+} from 'firebase/firestore';
 
 function App() {
   const [filter, setFilter] = useState(false);
@@ -41,18 +52,27 @@ function App() {
 
   return (
     <div className="container mx-auto my-10">
-      <div className="px-6 flow-root">
-        <label className="inline-flex relative items-center cursor-pointer">
+      <div className="px-6 flex justify-between">
+        <label className="inline-flex relative cursor-pointer">
           <input type="checkbox" value="" className="sr-only peer" onClick={() => setFilter(!filter)} />
-          <div className={`w-11 h-6 bg-[#333] peer-focus:outline-none ${filter ? 'peer-focus:ring-4 peer-focus:outline-none peer-focus:ring-amber-600 dark:peer-focus:ring-amber-500' : ''} rounded-full peer dark:bg-[#333] peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-amber-300`}></div>
+          <div className={`w-11 h-6 bg-[#333] peer-focus:outline-none ${filter ? 'peer-focus:ring-4 peer-focus:outline-none peer-focus:ring-amber-600 dark:peer-focus:ring-amber-500' : ''}
+            rounded-full peer dark:bg-[#333] peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px]
+            after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-amber-300`}></div>
         </label>
-        <span className="bg-pink-100 text-pink-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-pink-200 dark:text-pink-900 float-right">Vercel</span>
-        <span className="bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900 float-right">Firebase</span>
-        <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 float-right">React.js</span>
+        <Labels />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-6 p-6 mx-auto">
         <CreateNote createNote={createNote} />
-        {notes.map((note, i) => <Note key={note.id} id={note.id} note={note.text} favorite={note.favorite} timestamp={note.createdAt} deleteNote={deleteNote} updateNote={updateNote} />)}
+        {notes.map((note, i) => 
+          <Note
+            key={note.id}
+            id={note.id}
+            note={note.text}
+            favorite={note.favorite}
+            timestamp={note.createdAt}
+            deleteNote={deleteNote}
+            updateNote={updateNote}
+          />)}
       </div>
     </div>
   );
